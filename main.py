0ccbe71ba0fca63e2b6ea7c7b69a4f19d961497a
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-03-15 17:16:04 +0000

    final project completed

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    post = Post()
+    return render_template("index.html", post=post.data)
+
+@app.route('/post/<num>')
+def post_info(num):
+    post = Post().data
+    post_div = []
+    for data in post:
+        if data["id"] == int(num):
+            post_div.append(data)
+    print(post_div)
+    return render_template("post.html", post=post_div)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
